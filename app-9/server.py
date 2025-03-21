21c95fe21f2680ca792428dfee34f6e9944af808
Syr7-s <isa.sayar1725@gmail.com>
2025-03-21 23:14:50 +0000

    ingress

--- app-9/server.py
+++ app-9/server.py
@@ -5,7 +5,7 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         title = "Hello, Version 1"
-        bgcolor = "blue"
+        bgcolor = "green"
         self.render("template.html", title=title, bgcolor=bgcolor)
         print(self.request)
 
